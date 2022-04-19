f90b464e678462d376d1f9c70406ee7318cea1b8
Ankit0797 <53465886+Ankit0797@users.noreply.github.com>
2022-04-19 17:10:35 +0530

    Enter

--- idbi/settings.py
+++ idbi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pdd-3s5a!q5s$sx5pz)cnjx@tjn*u4gfzn4-b18sq=qrxyx=g@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
